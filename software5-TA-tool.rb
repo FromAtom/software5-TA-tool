3d6f2cf75d1573221367e8a3ac7ccfd864454dc7
Atom <fromatom@gmail.com>
2013-12-21 03:57:23 +0900

    add color

--- software5-TA-tool.rb
+++ software5-TA-tool.rb
@@ -4,6 +4,28 @@ require "optparse"
 require "fileutils"
 include FileUtils
 
+# 標準出力時に色をつけるためのクラス
+class TermColor
+  class << self
+    # 色を解除
+    def reset   ; c 0 ; end
+
+    # 各色
+    def red     ; c 31; end
+    def green   ; c 32; end
+    def yellow  ; c 33; end
+    def blue    ; c 34; end
+    def magenta ; c 35; end
+    def cyan    ; c 36; end
+    def white   ; c 37; end
+
+    # カラーシーケンスを出力する
+    def c(num)
+      print "\e[#{num.to_s}m"
+    end
+  end
+end
+
 class MakefileGenerater
   def initialize(target_dir_path, makefile_path)
     @target_dir_path = File.expand_path(target_dir_path)
@@ -70,8 +92,28 @@ OptionParser.new do |opts|
   end
 end
 
+def color_print(message)
+  TermColor.green
+  print message
+  TermColor.reset
+end
+
+def color_puts(message)
+  TermColor.green
+  puts message
+  TermColor.reset
+end
+
+
+color_print "setup Makefile generater..."
 makefile_generater = MakefileGenerater.new(config[:arg1], "./Makefile")
+color_puts "DONE"
+
+
+color_print "generate Makefile..."
 makefile_generater.generate
+color_puts "DONE"
+
 #
 #URRENT_DIR = File.expand_path(".")
 #URRENT_DIR_NAME = File.basename(Dir.getwd)
